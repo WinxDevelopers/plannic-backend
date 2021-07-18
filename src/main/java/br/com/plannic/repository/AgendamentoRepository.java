f7fb0e7683e4b02ad1d916a729cc30a53c9a5a96
Vitor Massao Sugai <massao.sg@gmail.com>
2021-07-18 19:21:21 -0300

    fix Query agendamento

--- src/main/java/br/com/plannic/repository/AgendamentoRepository.java
+++ src/main/java/br/com/plannic/repository/AgendamentoRepository.java
@@ -12,7 +12,11 @@ public interface AgendamentoRepository extends JpaRepository<Agendamento, Intege
     @Query(value = "" +
             "SELECT * " +
             "FROM public.agendamento a " +
-            "WHERE a.temponotificacao != 'N' and recorrenciainicio = current_date and (a.horainicio - ((cast(a.temponotificacao as integer) / 60 ) * interval '1 minute')) = cast(to_timestamp(to_char(now(),'HH24:MI:00'),'HH24:MI:00')as time)",
+            "WHERE a.temponotificacao != 'N' and ((recorrencia = 'N' and recorrenciainicio = current_date) or " +
+            "(recorrencia = 'dia' and recorrenciainicio <= current_date and recorrenciafim >= current_date) or " +
+            "(recorrencia = 'semana' and recorrenciainicio <= current_date and recorrenciafim >= current_date and (recorrenciainicio + interval '1 week') = current_date) or " +
+            "(recorrencia = 'mes' and recorrenciainicio <= current_date and recorrenciafim >= current_date and (recorrenciainicio + interval '1 month') = current_date)) " +
+            "and (a.horainicio - ((cast(a.temponotificacao as integer) / 60 ) * interval '1 minute')) = cast(to_timestamp(to_char(now(),'HH24:MI:00'),'HH24:MI:00')as time)",
             nativeQuery = true )
     List<Agendamento> findAllAgendamentosTelegram();
 }
